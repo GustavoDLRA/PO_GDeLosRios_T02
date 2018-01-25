1a612fa11154466d024e13e34084b43b173e05e0
GustavoDLRA <A01410922@itesm.mx>
2018-01-25 01:36:55 -0600

    Initial commit

---
+++ src/com/company/PCongelados.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class PCongelados {
+    protected String tempCongelacion;
+}
---
+++ src/com/company/Productos.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Productos {
+    protected String fechadeCaducidad;
+    protected String numLote;
+
+public Productos(String fechadeCaducidad, String numLote){
+    fechadeCaducidad = fechadeCaducidad;
+    numLote = numLote;
+}
+}
